00b340cfec8507c34d45e569916bdd6952dc2743
Nicholas Hollett <niax@niax.co.uk>
2014-10-12 22:16:16 +0100

    Rename Address -> Mailbox, Create Address enum to support Groups.

--- src/address.rs
+++ src/address.rs
@@ -4,34 +4,62 @@ use std::from_str::FromStr;
 use super::rfc5322::Rfc5322Parser;
 use super::header::FromHeader;
 
-/// Represents an RFC 822 address
+
+/// Represents an RFC 5322 Address
 #[deriving(PartialEq, Eq)]
-pub struct Address {
+pub enum Address {
+    /// A "regular" email address
+    AddressMailbox(Mailbox),
+}
+
+impl Address {
+    /// Shortcut function to make a new Mailbox with the given address
+    pub fn mailbox(address: String) -> Address {
+        AddressMailbox(Mailbox::new(address))
+    }
+
+    /// Shortcut function to make a new Mailbox with the address and given-name
+    pub fn mailbox_with_name(name: String, address: String) -> Address {
+        AddressMailbox(Mailbox::new_with_name(name, address))
+    }
+}
+
+impl fmt::Show for Address {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            AddressMailbox(ref mbox) => mbox.fmt(fmt),
+        }
+    }
+}
+
+/// Represents an RFC 5322 mailbox
+#[deriving(PartialEq, Eq)]
+pub struct Mailbox {
     /// The given name for this address
     pub name: Option<String>,
     /// The mailbox address
     pub address: String,
 }
 
-impl Address {
-    /// Create a new Address without a name
-    pub fn new(address: String) -> Address {
-        Address {
+impl Mailbox {
+    /// Create a new Mailbox without a display name
+    pub fn new(address: String) -> Mailbox {
+        Mailbox {
             name: None,
             address: address
         }
     }
 
-    /// Create a new Address with a name
-    pub fn new_with_name(name: String, address: String) -> Address {
-        Address {
+    /// Create a new Mailbox with a display name
+    pub fn new_with_name(name: String, address: String) -> Mailbox {
+        Mailbox {
             name: Some(name),
             address: address
         }
     }
 }
 
-impl fmt::Show for Address {
+impl fmt::Show for Mailbox {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         match self.name {
             Some(ref name) => write!(fmt, "\"{}\" <{}>", name, self.address),
@@ -40,8 +68,8 @@ impl fmt::Show for Address {
     }
 }
 
-impl FromStr for Address {
-    fn from_str(s: &str) -> Option<Address> {
+impl FromStr for Mailbox {
+    fn from_str(s: &str) -> Option<Mailbox> {
         AddressParser::new(s).parse_mailbox()
     }
 }
@@ -68,7 +96,7 @@ impl<'s> AddressParser<'s> {
 
         while !self.p.eof() {
             match self.parse_mailbox() {
-                Some(mailbox) => result.push(mailbox),
+                Some(mailbox) => result.push(AddressMailbox(mailbox)),
                 None => {},
             }
 
@@ -82,7 +110,7 @@ impl<'s> AddressParser<'s> {
         result
     }
 
-    pub fn parse_mailbox(&mut self) -> Option<Address> {
+    pub fn parse_mailbox(&mut self) -> Option<Mailbox> {
         // Push the current position of the parser so we can back out later
         self.p.push_position();
         let mut result = self.parse_name_addr();
@@ -92,14 +120,14 @@ impl<'s> AddressParser<'s> {
 
             let addr_spec = self.parse_addr_spec();
             if addr_spec.is_some() {
-                result = Some(Address::new(addr_spec.unwrap()));
+                result = Some(Mailbox::new(addr_spec.unwrap()));
             }
         }
 
         result
     }
 
-    fn parse_name_addr(&mut self) -> Option<Address> {
+    fn parse_name_addr(&mut self) -> Option<Mailbox> {
         // Find display-name
         let display_name = self.p.consume_phrase(false);
         self.p.consume_linear_whitespace();
@@ -112,8 +140,8 @@ impl<'s> AddressParser<'s> {
                 None
             } else {
                 match (display_name, addr) {
-                    (Some(name), Some(addr)) => Some(Address::new_with_name(name, addr)),
-                    (None, Some(addr)) => Some(Address::new(addr)),
+                    (Some(name), Some(addr)) => Some(Mailbox::new_with_name(name, addr)),
+                    (None, Some(addr)) => Some(Mailbox::new(addr)),
                     (_, _) => None,
                 }
             }
@@ -151,20 +179,20 @@ mod tests {
 
     #[test]
     fn test_address_to_string() {
-        let addr = Address::new("foo@example.org".to_string());
+        let addr = Mailbox::new("foo@example.org".to_string());
         assert_eq!(addr.to_string(), "<foo@example.org>".to_string());
 
-        let name_addr = Address::new_with_name("Joe Blogs".to_string(), "foo@example.org".to_string());
+        let name_addr = Mailbox::new_with_name("Joe Blogs".to_string(), "foo@example.org".to_string());
         assert_eq!(name_addr.to_string(), "\"Joe Blogs\" <foo@example.org>".to_string());
     }
 
     #[test]
     fn test_address_from_string() {
-        let addr = from_str::<Address>("\"Joe Blogs\" <joe@example.org>").unwrap();
+        let addr = from_str::<Mailbox>("\"Joe Blogs\" <joe@example.org>").unwrap();
         assert_eq!(addr.name.unwrap(), "Joe Blogs".to_string());
         assert_eq!(addr.address, "joe@example.org".to_string());
 
-        assert!(from_str::<Address>("Not an address").is_none());
+        assert!(from_str::<Mailbox>("Not an address").is_none());
     }
 
     #[test]
@@ -184,8 +212,8 @@ mod tests {
     fn test_address_list_parsing() {
         let mut parser = AddressParser::new("\"Joe Blogs\" <joe@example.org>, \"John Doe\" <john@example.org>");
         assert_eq!(parser.parse_address_list(), vec![
-            Address::new_with_name("Joe Blogs".to_string(), "joe@example.org".to_string()),
-            Address::new_with_name("John Doe".to_string(), "john@example.org".to_string()),
+            Address::mailbox_with_name("Joe Blogs".to_string(), "joe@example.org".to_string()),
+            Address::mailbox_with_name("John Doe".to_string(), "john@example.org".to_string()),
         ]);
     }
 
@@ -196,8 +224,8 @@ mod tests {
             "\"Joe Blogs\" <joe@example.org>, \"John Doe\" <john@example.org>".to_string());
         let addresses: Vec<Address> = header.get_value().unwrap();
         assert_eq!(addresses, vec![
-            Address::new_with_name("Joe Blogs".to_string(), "joe@example.org".to_string()),
-            Address::new_with_name("John Doe".to_string(), "john@example.org".to_string()),
+            Address::mailbox_with_name("Joe Blogs".to_string(), "joe@example.org".to_string()),
+            Address::mailbox_with_name("John Doe".to_string(), "john@example.org".to_string()),
         ]);
     }
 }
