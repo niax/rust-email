0fa8c344523283e34e7485a2d4e069c08df9187f
Paolo Barbolini <paolo@paolo565.org>
2019-12-24 20:27:39 +0100

    cargo clippy --examples

--- examples/parse_email.rs
+++ examples/parse_email.rs
@@ -11,12 +11,10 @@ fn main() {
     assert!(args.len() > 1);
     let msg_path = Path::new(&args[1]);
 
-    let mut file = File::open(&msg_path).ok().expect("can't open file");
+    let mut file = File::open(&msg_path).expect("can't open file");
     let raw_msg_bytes = {
         let mut rv: Vec<u8> = vec![];
-        file.read_to_end(&mut rv)
-            .ok()
-            .expect("can't read from file");
+        file.read_to_end(&mut rv).expect("can't read from file");
         rv
     };
     let raw_msg = String::from_utf8_lossy(&raw_msg_bytes);
